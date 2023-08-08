fcf4f58aade38057f21d47fc20e740b568fc3043
Yair <32864499+bluepostit@users.noreply.github.com>
2023-08-08 10:32:50 +0200

    added route 2

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 - `GET /` responds with HTML with a friendly message
 - `GET /api/about` responds with info about the company in JSON format
-- `GET /staff` responds with info about the employees
+- `GET /api/staff` responds with info about the employees
 - ....
 - `GET /contact` responds with contact page with form
 - `POST /contact` submits the form from the contact page, redirects to home page.
--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import path from "path";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const aboutData = {
+  name: "Cool Company",
+  description: "We do things well and make a difference",
+  established: 2000,
+};
+
 app.get("/", (req, res) => {
   console.log("got a request for /");
   // res.send("Hi there, everyone!");
@@ -11,6 +17,10 @@ app.get("/", (req, res) => {
   // Find the file index.html in this directory, and send it.
   const dirName = path.dirname(new URL(import.meta.url).pathname);
   res.sendFile(path.join(dirName, "/index.html"));
+});
+
+app.get("/api/about", (req, res) => {
+  res.json(aboutData);
 });
 
 app.listen(PORT, () => {
