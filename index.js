906c745564897c2bdf835e3ebd4cad1750c5fb4b
Yair <32864499+bluepostit@users.noreply.github.com>
2023-08-08 10:25:52 +0200

    first route sends html file

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import path from "path";
+
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.get("/", (req, res) => {
+  console.log("got a request for /");
+  // res.send("Hi there, everyone!");
+
+  // Find the file index.html in this directory, and send it.
+  const dirName = path.dirname(new URL(import.meta.url).pathname);
+  res.sendFile(path.join(dirName, "/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
